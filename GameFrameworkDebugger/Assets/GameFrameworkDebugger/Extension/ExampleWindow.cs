2017c2a6583102a723edcfc1e2d30047c6a5a5f9
swejackies <hongchengguo@szkuniu.com>
2017-08-22 17:50:12 +0800

    GameFrameworkDebugger

---
+++ GameFrameworkDebugger/Assets/GameFrameworkDebugger/Extension/ExampleWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public class ExampleWindow : ScrollableDebuggerWindowBase
+    {
+        protected override void OnDrawScrollableWindow()
+        {
+            GUILayout.Label("<b>GM</b>");
+            GUILayout.BeginVertical("box");
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("client gm.");
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndVertical();
+        }
+    }
+}
